670881a8dd02c4ae2e0e2ddea1fa0353ee5ff8dd
Jed Thompson <jedthompson@gmail.com>
2021-08-13 16:04:20 -0600

    runner up

---
+++ basic data types/nested_lists/nested_lists_learn.py
@@ -0,0 +1,21 @@
+
+def main():
+    number_students = int(input())
+    students = []
+    grades = []
+
+    for i in range(number_students):
+        name = input('Student Name: ')
+        grade = float(input("Grade: "))
+        grades.append(grade)
+        students.append([name, grade])
+
+    second_lowest_grade = sorted(grades)[1]
+    second_lowest_students = sorted(
+        [student[0] for student in students if student[1] == second_lowest_grade])
+    for student in second_lowest_students:
+        print(student)
+
+
+if __name__ == '__main__':
+    main()
