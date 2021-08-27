0389019d82c8a2436ca767b73a29495b7fe481b9
Jed Thompson <jedthompson@gmail.com>
2021-08-27 11:34:03 -0600

    capitalize

---
+++ Capitalize/capitalize_hacker.py
@@ -0,0 +1,15 @@
+s = input()
+for x in s[:].split():
+    s = s.replace(x, x.capitalize())
+print(s)
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     s = input()
+
+#     result = solve(s)
+
+#     fptr.write(result + '\n')
+
+#     fptr.close()
